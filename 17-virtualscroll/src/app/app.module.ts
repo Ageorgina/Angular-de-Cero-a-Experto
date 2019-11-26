377da86497aa2fe1910616d4f0a61fbc32bbe290
Ana Ortega <aortega@asgbpm.com>
2019-11-26 10:46:07 -0600

    Terminada 17

--- 17-virtualscroll/src/app/app.module.ts
+++ 17-virtualscroll/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ScrollingModule } from '@angular/cdk/scrolling'
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
+import { DragComponent } from './drag/drag.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaisesComponent } from './paises/paises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ScrollingModule
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 17-virtualscroll/src/app/virtual/virtual.component.ts
+++ 17-virtualscroll/src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -6,13 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./virtual.component.css']
 })
 export class VirtualComponent implements OnInit {
-  personas = Array(500).fill(0);
+  @ViewChild( CdkVirtualScrollViewport, {static: true} ) viewport: CdkVirtualScrollViewport;
+
+  personas = Array(1000).fill(0);
 
   constructor() {
     console.log(this.personas);
    }
 
   ngOnInit() {
+  }
+
+  irFinal() {
+    console.log('entro');
+    this.viewport.scrollToIndex(this.personas.length);
+  }
+  irInicio() {
+    console.log('entro');
+    this.viewport.scrollToIndex(this.personas[0]);
+  }
+  irMitad() {
+    console.log('entro');
+    this.viewport.scrollToIndex(this.personas.length / 2);
   }
 
 }
